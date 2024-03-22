20d2cb43d88aea66501659b3dd302d42ab0fdf2b
Aaliyah Rashid <aalimarier@gmail.com>
2024-03-22 11:24:45 -0400

    firebase, bloc, and repo architecture

--- moneyminder_v2/lib/main.dart
+++ moneyminder_v2/lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moneyminder_v2/app.dart';
+import 'package:moneyminder_v2/firebase_options.dart';
+import 'package:moneyminder_v2/simple_bloc_observer.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  Bloc.observer = SimpleBlocObserver();
   runApp(const MyApp());
 }
--- moneyminder_v2/lib/screens/home/views/home_screen.dart
+++ moneyminder_v2/lib/screens/home/views/home_screen.dart
@@ -1,6 +1,9 @@
 import 'dart:math';
+import 'package:expense_repository/expense_repository.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:moneyminder_v2/screens/add_expense/blocs/create_category_bloc/create_category_bloc.dart';
 import 'package:moneyminder_v2/screens/add_expense/views/add_expense.dart';
 import 'package:moneyminder_v2/screens/home/views/main_screen.dart';
 import 'package:moneyminder_v2/screens/stats/stats.dart';
@@ -13,7 +16,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   var widgetList = [
     MainScreen(),
     StatsScreen(),
@@ -26,75 +28,65 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(),
-      bottomNavigationBar: ClipRRect(
-        borderRadius: const BorderRadius.vertical(
-          top: Radius.circular(30)
-        ),
-        child: BottomNavigationBar(
-          onTap:(value) {
-            setState(() {
-              index = value;
-            });
-            print(value);
-          },
-          //fixedColor: Colors.red,
-          //backgroundColor: Colors.white,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          selectedItemColor: Colors.yellow,
-          elevation: 3,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(
-                CupertinoIcons.home,
-                color: index == 0 ? selectedItem : unselectedItem
-              ),
-              label: 'Home'
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                CupertinoIcons.graph_square_fill,
-                color: index == 1 ? selectedItem : unselectedItem
-              ), 
-              label: 'Stats'
-            )
-          ],
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute<void>(
-              builder: (BuildContext context) => const AddExpense(),
-            ),
-          );
-        },
-        shape: const CircleBorder(),
-        child: Container(
-          width: 60,
-          height: 60,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            gradient: LinearGradient(
-              colors:[
-                Theme.of(context).colorScheme.tertiary,
-                Theme.of(context).colorScheme.secondary,
-                Theme.of(context).colorScheme.primary
-              ],
-              transform: const GradientRotation(pi /4),
-            )
+        // appBar: AppBar(),
+        bottomNavigationBar: ClipRRect(
+          borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
+          child: BottomNavigationBar(
+            onTap: (value) {
+              setState(() {
+                index = value;
+              });
+              print(value);
+            },
+            //fixedColor: Colors.red,
+            //backgroundColor: Colors.white,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            selectedItemColor: Colors.yellow,
+            elevation: 3,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.home,
+                      color: index == 0 ? selectedItem : unselectedItem),
+                  label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.graph_square_fill,
+                      color: index == 1 ? selectedItem : unselectedItem),
+                  label: 'Stats')
+            ],
           ),
-          child: const Icon(
-            CupertinoIcons.add
-          ),
-        )
-      ),
-      body: index == 0
-        ? MainScreen()
-        : StatsScreen()
-    );
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (BuildContext context) => BlocProvider(
+                    create: (context) => CreateCategoryBloc(
+                      FirebaseExpenseRepo()
+                    ),
+                    child: const AddExpense(),
+                  ),
+                ),
+              );
+            },
+            shape: const CircleBorder(),
+            child: Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    colors: [
+                      Theme.of(context).colorScheme.tertiary,
+                      Theme.of(context).colorScheme.secondary,
+                      Theme.of(context).colorScheme.primary
+                    ],
+                    transform: const GradientRotation(pi / 4),
+                  )),
+              child: const Icon(CupertinoIcons.add),
+            )),
+        body: index == 0 ? MainScreen() : StatsScreen());
   }
 }
